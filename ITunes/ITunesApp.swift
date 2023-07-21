6bbb6a0e42c36f6a5aee972d9afdd3c5eebe090d
= <kumaravi651@gmail.com>
2023-07-21 14:57:23 +0530

    Initial Commit

---
+++ ITunes/ITunesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ITunesApp.swift
+//  ITunes
+//
+//  Created by Avinash Kumar on 21/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ITunesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
